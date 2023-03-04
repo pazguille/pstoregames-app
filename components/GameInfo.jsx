0074bcc98f94fc417f690e286d7178c211d22154
pazguille <guille87paz@gmail.com>
2023-03-04 15:22:12 -0300

    Update concept

--- components/GameInfo.jsx
+++ components/GameInfo.jsx
@@ -4,7 +4,17 @@ import GamePrice from '@/components/GamePrice.jsx';
 export default function GameInfo({ game }) {
   return (
     <div>
-      <h3 class="game-title"><a id={`detail-${game.id}`} href={`/game/${slugify(game.title)}_${game.id}`} class="link">{game.title}</a></h3>
+      <h3 class="game-title">
+        <a
+          id={`detail-${game.id}`}
+          href={game.concept ?
+            `https://store.playstation.com/es-ar/concept/${game.id}`
+            :
+            `/game/${slugify(game.title)}_${game.id}`}
+          class="link">
+            {game.title}
+        </a>
+      </h3>
       {game.platforms ? <p class="game-by">{game.platforms.join(' | ')}</p> : null}
       <GamePrice game={game} />
     </div>
